bb7cab6f576c61cab95a8e59669f7c4e2a14ca35
Carl Geiger <geigercarlp@gmail.com>
2024-03-12 17:38:01 -0400

    text_node_to_html_node function added.

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text  # Text content of node
@@ -9,3 +11,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    supported_text_types = ["text", "bold", "italic", "code", "link", "image"]
+    if text_node.text_type not in supported_text_types:
+        raise ValueError(f"{text_node.text_type} is not a supported text type.")
+    if text_node.text_type == "text":
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == "bold":
+        return LeafNode('b', text_node.text)
+    if text_node.text_type == "italic":
+        return LeafNode('i', text_node.text)
+    if text_node.text_type == "code":
+        return LeafNode('code', text_node.text)
+    if text_node.text_type == "link":
+        return LeafNode('a', text_node.text, {"href": text_node.url})
+    if text_node.text_type == "image":
+        return LeafNode('img', '', {"src": text_node.url, "alt": text_node.text})
+    return None
